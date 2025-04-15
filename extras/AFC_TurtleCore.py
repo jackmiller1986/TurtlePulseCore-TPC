c9c9d2d67e285211700aba66cea1aa0eafb445c9
Basdiani <95381619+Basdiani@users.noreply.github.com>
2025-04-15 19:44:45 +0200

    Update AFC_TurtleCore.py

--- extras/AFC_TurtleCore.py
+++ extras/AFC_TurtleCore.py
@@ -3,22 +3,29 @@
 # Copyright (C) 2024 Armored Turtle
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
+
 from configparser import Error as error
+
+# Import der Standardklasse
 try:
     from extras.AFC_BoxTurtle import afcBoxTurtle
-except:
-    raise error("Error trying to import AFC_BoxTurtle, please rerun install-afc.sh script in your AFC-Klipper-Add-On directory then restart klipper")
+except Exception as e:
+    raise error(
+        "Fehler beim Import von AFC_BoxTurtle.\n"
+        "Bitte führe das Installationsskript erneut aus: ./install-afc.sh\n"
+        f"Originalfehler: {str(e)}"
+    )
 
+# Spezialklasse TurtleCore, erbt von afcBoxTurtle
 class afcTurtleCore(afcBoxTurtle):
     def __init__(self, config):
         super().__init__(config)
-        self.type = config.get('type', 'Turtle_Core')
+        self.type = config.get('type', 'TurtleCore')
 
     def handle_connect(self):
         """
         Handle the connection event.
-        This function is called when the printer connects. It looks up AFC info
-        and assigns it to the instance variable `self.AFC`.
+        Wird aufgerufen, wenn der Drucker verbunden ist.
         """
         super().handle_connect()
 
@@ -32,5 +39,18 @@ class afcTurtleCore(afcBoxTurtle):
 
         self.logo_error = '<span class=error--text>TurtleCore Not Ready</span>\n'
 
+# Diese Methode wird von Klipper beim Laden des Moduls verwendet
 def load_config_prefix(config):
-    return afcTurtleCore(config)
+    try:
+        section_type, section_name = config.get_name().split(None, 1)
+    except ValueError:
+        raise error(f"Ungültiger Abschnittsname: {config.get_name()} (erwarte: [AFC_BoxTurtle <Name>])")
+
+    if section_type == "AFC_BoxTurtle":
+        type_value = config.get('type', '').lower()
+        if type_value == "turtlecore":
+            return afcTurtleCore(config)
+        else:
+            return afcBoxTurtle(config)
+
+    raise error(f"Unbekannter Abschnittstyp: {section_type}")
